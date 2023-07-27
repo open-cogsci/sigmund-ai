f6896e1ff231480d9341fa67434f9e4300ed4a8f
smathot <s.mathot@cogsci.nl>
2023-07-27 16:50:39 +0200

    Bump to 0.1.5

--- heymans.py
+++ heymans.py
@@ -9,7 +9,7 @@ import logging
 import time
 import config
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 logger = logging.getLogger('heymans')
 logging.basicConfig(level=logging.INFO, force=True)
 
