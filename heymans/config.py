eafb410a788683f673672505fb98cc439188b70d
smathot <s.mathot@cogsci.nl>
2023-12-10 15:14:52 +0100

    Allow flask to be configured through environment variables

--- heymans/config.py
+++ heymans/config.py
@@ -6,13 +6,13 @@ ai_name = 'Sigmund'
 
 # SERVER
 # The external server address, that is, the URL that users visit
-server_url = 'http://127.0.0.1:5000'
+server_url = os.environ.get('FLASK_SERVER_URL', 'http://127.0.0.1:5000')
 # The port at which the flask server is running internally. This can be
 # different from the server URL if the app is running behind a proxy that
 # redirects
-flask_port = 5000
+flask_port = int(os.environ.get('FLASK_PORT', 5000))
 # The flask host arhument where all 0s means listen to all incoming addresses
-flask_host = '0.0.0.0'
+flask_host = os.environ.get('FLASK_HOST', '0.0.0.0')
 # The secret key is used for logging in. This should be a long and arbitrary
 # string that is hard to guess. This should not be shared
 flask_secret_key = os.environ.get('FLASK_SECRET_KEY', None)
