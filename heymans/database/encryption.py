c5a3d4ea1d077c3e7a3602cfe4c7c4ec801cdf0c
smathot <s.mathot@cogsci.nl>
2024-01-14 11:23:55 +0100

    Allow encryption manager to work in dummy mode

--- heymans/database/encryption.py
+++ heymans/database/encryption.py
@@ -1,13 +1,28 @@
 from cryptography.fernet import Fernet
+import types
+import logging
+logger = logging.getLogger('heymans')
+
 
 class EncryptionManager:
-    def __init__(self, encryption_key: [str, bytes]):
+    """Handles encryption and decryption of messages and attachments. The
+    encryption key is derived from the user's password.
+    """
+    def __init__(self, encryption_key: [str, bytes, types.NoneType]):
+        if encryption_key is None:
+            logger.warning('no encryption key provided')
+            self.fernet = None
+            return
         if isinstance(encryption_key, str):
             encryption_key = encryption_key.encode()
         self.fernet = Fernet(encryption_key)
 
     def encrypt_data(self, data) -> bytes:
+        if self.fernet is None:
+            return data.encode('utf-8')
         return self.fernet.encrypt(data)
 
     def decrypt_data(self, data: bytes):
+        if self.fernet is None:
+            return data.decode('utf-8')
         return self.fernet.decrypt(data)
