1c9208fdbbea22c14f33aa9108dd20f9937376a6
Sebastiaan Mathot <s.mathot@cogsci.nl>
2025-11-12 18:27:36 +0100

    Fix foundation document unit tests

--- tests/expensive/test_foundation_document_topics.py
+++ tests/expensive/test_foundation_document_topics.py
@@ -6,7 +6,7 @@ logger = logging.getLogger('sigmund')
 logging.basicConfig(level=logging.INFO, force=True)
 
 
-class BaseExpensiveTest(unittest.TestCase):
+class TestFoundationDocumentTopics(unittest.TestCase):
     
     def setUp(self):
         from sigmund.database.models import drop_db, init_db
@@ -14,10 +14,11 @@ class BaseExpensiveTest(unittest.TestCase):
         init_db()
         config.max_tokens_per_hour = float('inf')
         config.log_replies = True
+        config.search_enabled = True
         self.sigmund = Sigmund(user_id='pytest', model_config='dummy',
                                foundation_document_topics=['opensesame'])
         
-    def test_dummy(self):
+    def test(self):
         for result in self.sigmund.send_user_message('dummy query'):
             print(result)
         assert any(d['topic'] == 'opensesame'
