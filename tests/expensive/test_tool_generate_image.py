39dd8c7b550c060d520396dbab331be8018b0452
Sebastiaan Mathot <s.mathot@cogsci.nl>
2026-02-19 14:16:18 +0100

    Fix expensive unit tests

--- tests/expensive/test_tool_generate_image.py
+++ tests/expensive/test_tool_generate_image.py
@@ -1,7 +1,12 @@
 from .expensive_test_utils import BaseExpensiveTest
+from sigmund import config
 
 
 class TestGenerateImage(BaseExpensiveTest):
+    
+    def setUp(self):
+        config.settings_default['tool_generate_image'] = 'true'
+        super().setUp()    
     
     def _test_tool(self):
         query = 'Can you generate a small image of a black cat?'
--- tests/expensive/test_tool_search_google_scholar.py
+++ tests/expensive/test_tool_search_google_scholar.py
@@ -1,7 +1,12 @@
 from .expensive_test_utils import BaseExpensiveTest
+from sigmund import config
 
 
 class TestToolsGoogleScholar(BaseExpensiveTest):
+    
+    def setUp(self):
+        config.settings_default['tool_search_google_scholar'] = 'true'
+        super().setUp()        
     
     def _test_tool(self):
         query = 'Can you search Google Scholar for articles about pupillometry in psychology? What is the title of the review article by Mathôt?'
