71f6ecb23e090f867df543cd8e69745532e4f9ac
Sebastiaan Mathot <s.mathot@cogsci.nl>
2026-05-09 11:38:32 +0200

    Fix hourly token limit
    - Bump to 0.25.1

--- sigmund/config.py
+++ sigmund/config.py
@@ -118,8 +118,7 @@ mistral_kwargs = {}
 # that it corresponds to 10 dollars over a 7 day range.
 # The maximum number of tokens that can be consumed per hour by the answer
 # model.
-# hourly_token_limit = 2e6
-hourly_token_limit = 2e1
+hourly_token_limit = 2e6
 hourly_limit_exceeded_message = 'Sorry, I can\'t reply right now, because you have reached the hourly usage limit. Don\'t worry, within at most an hour we\'ll be able to talk to again.\n\nIn the meanwhile, why not review our [fair-use policy](/fair-use)? This includes suggestions to keep usage under control in the future. 🚦⏳🤗'
 soft_token_limit = 1e7
 hard_token_limit = 10e7
